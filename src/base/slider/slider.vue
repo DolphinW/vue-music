ae85d928ee209416416342eea35a7719d7add087
wangzy <2062039357@qq.com>
2018-02-24 09:21:16 +0800

    完善sliders

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -6,7 +6,7 @@
       </slot>
     </div>
     <div class="dots">
-
+      <span class="dot" v-for="(dot,index) in dots" :key="index" :class="{active:currentPageIndex==index}"></span>
     </div>
   </div>
 </template>
@@ -18,6 +18,12 @@
 
   export default {
     name: "slider",
+    data(){
+      return {
+        dots:[],
+        currentPageIndex:0
+      }
+    },
     props: {
       loop: {
         type: Boolean,
@@ -33,14 +39,28 @@
       }
     },
     mounted() {
-      setTimeout(()=>
-      {
+      this.$nextTick(()=>{
         this.setSliderWidth()
+        this.initDots()
         this.initSlider()
-      },20)
+        if(this.autoPlay){
+          this._play()
+        }
+      })
+      window.addEventListener('resize',()=>{
+        this.setSliderWidth(true)
+      })
+      // 也可以使用这个
+      // setTimeout(()=>
+      // {
+      //
+      // },20)
+    },
+    destroyed(){
+      clearTimeout(this.timer)
     },
     methods: {
-      setSliderWidth() {
+      setSliderWidth(isResize) {
         this.children = this.$refs.sliderGroup.children;
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth;
@@ -50,13 +70,13 @@
           child.style.width = sliderWidth + "px"
           width += sliderWidth
         }
-        if (this.loop) {
+        if (this.loop && !isResize) {
           width += 2 * sliderWidth
         }
         this.$refs.sliderGroup.style.width = width + "px"
       },
       initSlider() {
-        new BScroll(this.$refs.slider,{
+        this.slider=new BScroll(this.$refs.slider,{
           scrollX: true,
           scrollY: false,
           momentum: false,
@@ -65,6 +85,30 @@
           snapThreshold: 0.3,
           snapSpeed: 400
         })
+        // 滚动改变currentPageIndex
+        this.slider.on('scrollEnd',()=>{
+          let pageIndex=this.slider.getCurrentPage().pageX;
+          if(this.loop){
+            pageIndex-=1
+          }
+          this.currentPageIndex=pageIndex;
+          if(this.autoPlay){
+            clearTimeout(this.timer)
+            this._play();
+          }
+        })
+      },
+      initDots(){
+        this.dots=this.children.length;
+      },
+      _play(){
+        this.timer=setTimeout(()=>{
+          let pageIndex=this.currentPageIndex+1;
+          if(this.loop){
+            pageIndex+=1
+          }
+          this.slider.goToPage(pageIndex,0,400)
+        },this.interval)
       }
     }
   }
